faf629bdb3912cfdacd071ab235b520e2fc004e1
Dason Kurkiewicz <dasonk@gmail.com>
2015-12-19 16:29:57 -0600

    Added obfuscated version of the function

--- R/displayR.R
+++ R/displayR.R
@@ -1,6 +1,6 @@
 #' Display R
 #'
-#' Displays a capital R
+#' Displays a capital R.
 #'
 #' @export
 #' @examples
@@ -21,4 +21,23 @@ displayR <- function(){
     }
     cat("\n")
     cat(ans, sep= "")
+}
+
+
+#' Display R
+#'
+#' Displays a capital R. Same outcome as displayR() but the code
+#' has be obfuscated. That isn't to say that the original code
+#' is very clear...
+#'
+#' @export
+#' @examples
+#' displayRobfuscated()
+displayRobfuscated <- function(){
+    h=character;r=rep;a=b=h(0);p=options()$width%/%2-5;n="
+    ";j=r(toupper(substring(mode(a),4,4)),sum(r(5:9,2)+1)-3)
+    k=r(5:9,2);k[4:5]=7;k=cumsum(k+1);j[k]=n;m=paste(h(1), 
+                                                     h(1));s=c(0,k[-10])+1;j[c(16:17,24:26,32:33,46:47,53:55,
+                                                                             61:64,70:74)]=m;for(i in 1:10)a=c(a,r(m,p),j[s[i]:k[i]])
+    cat(c(n,a),sep=b)
 }
